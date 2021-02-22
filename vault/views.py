5f1b865068fc6bb321f0ba9ee3c904781ff0dc3b
Gurshan Brar <meltedgold7@gmail.com>
2021-02-22 13:17:58 -0800

    validation

--- vault/views.py
+++ vault/views.py
@@ -2,6 +2,8 @@ from django.http.response import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 import face_recognition
 from . import forms, urls
 from .models import Group, Member
@@ -13,8 +15,10 @@ from io import BytesIO
 # Create your views here.
 letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
+
 def index(request):
     return render(request, 'index.html')
+
 
 def create_count(request):
     if request.method == 'POST':
@@ -26,7 +30,6 @@ def create_count(request):
         form = forms.CreateCountForm()
 
     return render(request, 'create-count.html', {'form': form})
-
 
 
 def create_success(request, group_id=None):
@@ -42,31 +45,48 @@ def create(request, member_count=None):
         print('no member count')
         raise Http404
     if request.method == 'POST':
-        form = forms.CreateForm(request.POST,request.FILES, member_count=member_count)
+        form = forms.CreateForm(
+            request.POST, request.FILES, member_count=member_count)
         if form.is_valid():
+            object_list = []
+            error_raised = False
             # do this before and save it
 
             # get the group id
             group_id = ''
             for i in range(7):
-                group_id += letters[random.randint(0, 61)] 
+                group_id += letters[random.randint(0, 61)]
             # get the meeting url
             meeting_url = form.cleaned_data['meeting_url']
-            group = Group.objects.create(group_id = group_id, meeting_url = meeting_url)
+            group = Group(
+                group_id=group_id, meeting_url=meeting_url)
+            object_list.append(group)
             # take care of getting member count
             for i in range(member_count):
                 name = form.cleaned_data[f'name_{i}']
 
                 image = form.cleaned_data[f'image_{i}']
-                person = (face_recognition.face_encodings(face_recognition.load_image_file(image))[0])
-                string_person = base64.binascii.b2a_base64(person).decode("ascii")
+                try:
+                    person = (face_recognition.face_encodings(
+                        face_recognition.load_image_file(image))[0])
+                except:
+                    form.add_error(f'image_{i}', ValidationError(
+                        _('Please enter a human face!'), code='invalid'))
+                    error_raised = True
+                    continue
+                string_person = base64.binascii.b2a_base64(
+                    person).decode("ascii")
 
-                member = Member.objects.create(name=name, encoded_file = string_person, group = group)
-            return redirect('create-success', group_id=group_id)
+                member = Member(
+                    name=name, encoded_file=string_person, group=group)
+                object_list.append(member)
+            if not error_raised:
+                for model in object_list:
+                    model.save()
+                return redirect('create-success', group_id=group_id)
 
     else:
         form = forms.CreateForm(member_count=member_count)
-
     return render(request, 'create.html', {'form': form})
 
 
@@ -78,7 +98,7 @@ def join(request):
             group_id = form.cleaned_data['group_id']
             name = form.cleaned_data['name']
             image_64 = form.cleaned_data['image']
-            
+
             image = BytesIO(base64.b64decode(image_64))
             try:
                 group = Group.objects.filter(group_id__exact=group_id)[0]
@@ -87,20 +107,25 @@ def join(request):
             members = group.member_set.all()
             matching_names = members.filter(name__exact=name)
             try:
-                user = face_recognition.face_encodings(face_recognition.load_image_file(image))[0]
+                user = face_recognition.face_encodings(
+                    face_recognition.load_image_file(image))[0]
             except:
                 return redirect('join-failed')
             for i in matching_names:
-                test = np.frombuffer(base64.binascii.a2b_base64(i.encoded_file.encode("ascii")))
+                test = np.frombuffer(base64.binascii.a2b_base64(
+                    i.encoded_file.encode("ascii")))
                 if face_recognition.compare_faces([test], user):
-                    face_distances = face_recognition.face_distance([test], user)
-                    face_match_percentage = (1-face_distances)*100
+                    face_distances = face_recognition.face_distance(
+                        [test], user)
+                    face_match_percentage = (1 - face_distances) * 100
                     for i, face_distance in enumerate(face_distances):
-                        print("The test image has a distance of {:.2} from known image #{}".format(face_distance, i))
+                        print("The test image has a distance of {:.2} from known image #{}".format(
+                            face_distance, i))
 
-                        print("- comparing with a tolerance of 0.6? {}".format(face_distance < 0.6))
-                        
-                        x = np.round(face_match_percentage,4)
+                        print(
+                            "- comparing with a tolerance of 0.6? {}".format(face_distance < 0.6))
+
+                        x = np.round(face_match_percentage, 4)
                         print(x)
                         if x > 55:
                             return redirect(f'{group.meeting_url}')
@@ -108,10 +133,8 @@ def join(request):
     else:
         form = forms.JoinForm()
 
-
     return render(request, 'join.html', {'form': form})
+
 
 def join_failed(request):
     return render(request, 'join-failed.html')
- 
-
